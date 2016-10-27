e44c999eea25280967805bd0d63dac00d1d4d7d8
Kingshuk Basak <kingshuk@fusioncharts.com>
2016-10-27 19:19:29 +0530

    Filters now are applied recursively for array of filters. Also if a data is modified, its subsequent child are changed

--- src/multicharting.datamanager.js
+++ src/multicharting.datamanager.js
@@ -1,38 +1,113 @@
 var	dataStore = {},
+	linkStore = {},
 	idCount = 0,
 	// Constructor class for dataManager.
 	dataManager = function (JSONData, id) {
     	var manager = this;
     	manager.addData(JSONData, id);
 	},
-	proto = dataManager.prototype;
+	proto = dataManager.prototype,
+	updataData = function (id) {
+		var i,
+			linkData = linkStore[id],
+			parentData = datastore[id],
+			len = linkData.length,
+			info;
+
+		for (i = 0; i < len; i++) {
+			info = linkData[i];
+
+			linkId = info.link;
+			filter = info.filter;
+
+			datastore[linkId] = filter(parentData);
+
+			if (linkStore[linkId]) {
+				updataData(linkId);
+			}
+		}
+	};
 
 // Function to add data in the data store
 proto.addData = function (JSONData, id) {
-	var manager = this,
-		oldId = manager.id,
+	var data = this,
+		oldId = data.id,
 		oldJSONData = dataStore[oldId] || [];
 
 	if (oldId && id) {
 		dataStore[id] = oldJSONData.concat(JSONData || []);
-		manager.deleteData();
-		manager.id = id;
+		delete dataStore[oldId];
+
+		if (linkStore[oldId]) {
+			linkStore[id] = linkStore[oldId];
+			delete linkStore[oldId];
+		}
 	}
 	else {
 		id = oldId ? oldId : (id || 'dataStore' + idCount ++);
 		dataStore[id] = oldJSONData.concat(JSONData || []);
-		manager.id = id;
+	}
+	data.id = id;
+
+	if (linkStore[id]) {
+		updataData(id)
 	}
 };
 
 // Function to get data from the data store after applying filters
 proto.getData = function (filters) {
-	return (filters && filters(dataStore[this.id]));
+	var data = this,
+		id = data.id;
+	// If no parameter is present then return the unfiltered data.
+	if (!filters) {
+		return dataStore[id];
+	}
+	// If parameter is an array of filter then return the filtered data after applying the filter over the data.
+	else if (filters instanceof Array) {
+		let result = [],
+			i,
+			newData,
+			linkData,
+			newId,
+			len = filters.length;
+
+		for (i = 0; i < len; i++) {
+			newData = new dataManager(filters[i](dataStore[id]));
+			newId = newData.id;
+
+			dataStore[newId] = newData;
+			result.push(newData);
+
+			//Pushing the id of child class in the parent classes.
+			linkData = linkStore[id] || (linkStore[id] = {
+				link : [],
+				filter : []
+			});
+			linkData.link.push(newId);
+			linkData.filter.push(filters[i]);
+
+			// setting the current id as the newID so that the next filter is applied on the child data;
+			id = newId;
+		}
+		return result;
+	}
 };
 
-// Function to delete the current data from the datastore
+// Function to delete the current data from the datastore and also all its childs recursively
 proto.deleteData = function () {
-	return (delete dataStore[this.id]);
+	var data = this;
+		id = data.id;
+		linkData = linkStore[id];
+
+	if (linkData) {
+		let i,
+			len = linkData.length;
+		for (i = 0; i < len; i ++) {
+			data.deleteData(linkData[i]);
+		}
+		delete linkStore[id];
+	}
+	return (delete dataStore[id]);
 };
 
 // Function to get the id of the current data
@@ -42,7 +117,9 @@ proto.getID = function () {
 
 // Function to modify data
 proto.modifyData = function (JSONData, id) {
-	var manager = this;
-	manager.deleteData();
-	manager.addData(JSONData, id);
+	var data = this,
+		id = data.id;
+
+	dataStore[id] = [];
+	data.addData(JSONData, id);
 };
