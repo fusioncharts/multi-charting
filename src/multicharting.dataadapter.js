60193bb963e860385f70bb3588178daba02dd8ee
anindya-fusioncharts <anindya@fusioncharts.com>
2016-11-07 18:44:44 +0530

    fix extend2 initialization

--- src/multicharting.dataadapter.js
+++ src/multicharting.dataadapter.js
@@ -10,8 +10,8 @@
     MultiCharting.prototype.dataadapter = function () {
         return convertData(arguments);
     };
-    var extend2 = MultiCharting.lib.extend2,
-        merge = MultiCharting.lib.merge;
+    var extend2 = MultiCharting.prototype.lib.extend2,
+        merge = MultiCharting.prototype.lib.merge;
     //function to convert data, it returns fc supported JSON
     function convertData() {
         var argument = arguments[0] || {},
