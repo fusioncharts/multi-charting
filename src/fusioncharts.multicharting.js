4c01997714c7d1b0621bc1b7b174fd626d38071d
anindya-fusioncharts <anindya@fusioncharts.com>
2016-11-07 13:40:10 +0530

    create chart api added

--- src/fusioncharts.multicharting.js
+++ src/fusioncharts.multicharting.js
@@ -16,3 +16,23 @@ multiProto.createDataStore = function () {
 multiProto.createDataProcessor = function () {
 	return new dataProcessor(arguments);
 }
+
+multiProto.createChart = function () {
+	var argument = arguments && arguments[0],
+		chartType = argument && argument.type,
+		container = argument && argument.container,
+		configuration = argument && argument.configuration,
+		callbackFN = argument && argument.callbackFN,
+		DATA = argument && argument.dataStore,
+		chartConfig = {},
+		dataSource = {};
+	dataSource = convertData(DATA, configuration, callbackFN);
+	chartConfig.type = chartType;
+	chartConfig.renderAt = container;
+	chartConfig.dataFormat = 'json';
+	chartConfig.dataSource = dataSource
+	FusionCharts.ready(function () {
+	    var chart = new FusionCharts(chartConfig);
+	    chart.render();
+	});
+};
