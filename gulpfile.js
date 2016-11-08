97ace7060029ed5aa1f8ce3bd11708189cad7c70
Rohit Kumar <rohit.kr001@gmail.com>
2016-11-08 21:07:35 +0530

    Build script modified

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,26 @@ var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	sourcemaps = require('gulp-sourcemaps'),
 	jshint = require('gulp-jshint'),
-	del = require('del'),
+	// del = require('del'),
     packageJSON  = require('./package'),
     stylish = require('jshint-stylish'),
     uglify = require('gulp-uglify'),
     pump = require('pump'),
+    rename = require('gulp-rename'),
+    rm = require('gulp-rimraf'),
     argArr = process.argv,
     argLen = argArr.length,
     jshintConfig = packageJSON.jshintConfig,
     BUILD_DIR = 'out/', // Name of the directory where to create the build files
     BUILD_FILE_NAME = 'fusioncharts.multicharting.js', // Name of the build file
     files = [
-		'src/fusioncharts.multicharting.js',
-		'src/multicharting.lib.js',
-		'src/multicharting.datastore.js',
-		'src/multicharting.dataprocessor.js',
-		'src/multicharting.dataadapter.js',
-		'src/multicharting.createchart.js',
-		'src/multicharting.matrix.js'
+		'src/fusioncharts.multicharting.js'
+		// 'src/multicharting.lib.js',
+		// 'src/multicharting.datastore.js',
+		// 'src/multicharting.dataprocessor.js',
+		// 'src/multicharting.dataadapter.js',
+		// 'src/multicharting.createchart.js',
+		// 'src/multicharting.matrix.js'
 	],
     ARGUMENTS = {},
 	arg;
@@ -38,12 +40,15 @@ if (argLen > 2) {
 
 // Clean the out directory
 gulp.task('clean', function () {
-	return del([BUILD_DIR]);
+	// console.log("\n\n Cleaning \n\n");
+	return gulp.src(BUILD_DIR).pipe(rm());
+	// return del([BUILD_DIR + '/*']);
 });
 
 // Run lint on the source
 gulp.task('lint', function() {
-	return gulp.src(ARGUMENTS.file || files)
+	// console.log("\n\n Linging \n\n");
+	return gulp.src(files)
     	.pipe(jshint(jshintConfig))
 		.pipe(jshint.reporter('jshint-stylish'))
 		.pipe(jshint.reporter('fail'));
@@ -51,6 +56,7 @@ gulp.task('lint', function() {
 
 // Build only the source file
 gulp.task('build-source', ['clean'], function () {
+	// console.log("\n\n build-source \n\n");
 	return gulp.src(files)
 		.pipe(sourcemaps.init())
 		.pipe(concat(BUILD_FILE_NAME))
@@ -60,7 +66,13 @@ gulp.task('build-source', ['clean'], function () {
 
 // Compress the source file
 gulp.task('compress', function (cb) {
-  	pump([gulp.src(BUILD_DIR + BUILD_FILE_NAME), uglify(), gulp.dest(BUILD_DIR)], cb);
+	// console.log("\n\n Compress \n\n");
+  	// pump([gulp.src(BUILD_DIR + BUILD_FILE_NAME), uglify(), gulp.dest(BUILD_DIR)], cb);
+	return gulp.src(BUILD_DIR + BUILD_FILE_NAME)
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest(BUILD_DIR));
+
 });
 
 // Build the source and minified files
