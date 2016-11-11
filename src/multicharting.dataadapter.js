198e80f6b7e650c3b71c7c80ae13b2233e5d3c82
anindya-fusioncharts <anindya@fusioncharts.com>
2016-11-11 15:01:02 +0530

    time series fc json issue fix

--- src/multicharting.dataadapter.js
+++ src/multicharting.dataadapter.js
@@ -89,30 +89,31 @@
                                 lenData,
                                 i,
                                 j;
-                            json.datasets = [];
-                            json.datasets[0] = {};
-                            json.datasets[0].category = {};
-                            json.datasets[0].category.data = [];
+                            json.chart = {};
+                            json.chart.datasets = [];
+                            json.chart.datasets[0] = {};
+                            json.chart.datasets[0].category = {};
+                            json.chart.datasets[0].category.data = [];
                             for (i = 0, lenDimension =  configuration.dimension.length; i < lenDimension; i++) {
                                 indexMatch = jsonData[0].indexOf(configuration.dimension[i]);
                                 if (indexMatch != -1) {
                                     for (j = 1, lenData = jsonData.length; j < lenData; j++) {
-                                        json.datasets[0].category.data.push(jsonData[j][indexMatch]);
+                                        json.chart.datasets[0].category.data.push(jsonData[j][indexMatch]);
                                     }
                                 }
                             }
-                            json.datasets[0].dataset = [];
-                            json.datasets[0].dataset[0] = {};
-                            json.datasets[0].dataset[0].series = [];
+                            json.chart.datasets[0].dataset = [];
+                            json.chart.datasets[0].dataset[0] = {};
+                            json.chart.datasets[0].dataset[0].series = [];
                             for (i = 0, lenMeasure = configuration.measure.length; i < lenMeasure; i++) {
                                 indexMatch = jsonData[0].indexOf(configuration.measure[i]);
                                 if (indexMatch != -1) {
-                                    json.datasets[0].dataset[0].series[i] = {  
+                                    json.chart.datasets[0].dataset[0].series[i] = {  
                                         'name' : configuration.measure[i],                              
                                         'data': []
                                     };
                                     for(j = 1, lenData = jsonData.length; j < lenData; j++) {
-                                        json.datasets[0].dataset[0].series[i].data.push(jsonData[j][indexMatch]);
+                                        json.chart.datasets[0].dataset[0].series[i].data.push(jsonData[j][indexMatch]);
                                     }
                                 }
                             }
@@ -153,7 +154,7 @@
 
         if (jsonData && configuration) {
             dataArray = generalDataFormat(jsonData, configuration);
-            json = jsonCreator(dataArray, configuration);
+            json = jsonCreator(dataArray, configuration);          
             json = (predefinedJson && extend2(json,predefinedJson)) || json;    
             return (callbackFN && callbackFN(json)) || json;    
         }
