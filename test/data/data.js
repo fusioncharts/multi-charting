6f9ff8f03ba5e4541dc686f5285da8d46c1f12f7
Rohit Kumar <rohit.kr001@gmail.com>
2016-11-24 12:30:47 +0530

    Add sample for datastore

---
+++ test/data/data.js
@@ -0,0 +1,31 @@
+data = [
+	["Product", "State", "Months", "Year", "Sales"],
+	["Tea", "New York", "Jan", "2013", "9550"],
+	["Tea", "New York", "Feb", "2013", "12446"],
+	["Tea", "New York", "Mar", "2013", "16939"],
+	["Tea", "New York", "Apr", "2013", "9886"],
+	["Tea", "New York", "May", "2013", "15317"],
+	["Tea", "New York", "Jun", "2013", "12651"],
+	["Tea", "New York", "Jul", "2013", "16486"],
+	["Tea", "New York", "Aug", "2013", "12501"],
+	["Tea", "New York", "Sep", "2013", "12863"],
+	["Tea", "New York", "Oct", "2013", "16924"],
+	["Tea", "New York", "Nov", "2013", "14885"],
+	["Tea", "New York", "Dec", "2013", "9504"]
+];
+
+
+data2 = [
+	["Espresso", "New York", "Jan", "2013", "12451"],
+	["Espresso", "New York", "Feb", "2013", "14763"],
+	["Espresso", "New York", "Mar", "2013", "11748"],
+	["Espresso", "New York", "Apr", "2013", "13067"],
+	["Espresso", "New York", "May", "2013", "15864"],
+	["Espresso", "New York", "Jun", "2013", "12687"],
+	["Espresso", "New York", "Jul", "2013", "9911"],
+	["Espresso", "New York", "Aug", "2013", "12935"],
+	["Espresso", "New York", "Sep", "2013", "10759"],
+	["Espresso", "New York", "Oct", "2013", "9050"],
+	["Espresso", "New York", "Nov", "2013", "12860"],
+	"Espresso", "New York", "Dec", "2013", "13947"
+]
