5fd8c75370c80eb1d98106fe5acc73ca569ab85b
Rohit Kumar <rohit.kr001@gmail.com>
2016-11-24 13:21:19 +0530

    Sample for data added

--- test/data/data.js
+++ test/data/data.js
@@ -1,31 +1,174 @@
 data = [
-	["Product", "State", "Months", "Year", "Sales"],
-	["Tea", "New York", "Jan", "2013", "9550"],
-	["Tea", "New York", "Feb", "2013", "12446"],
-	["Tea", "New York", "Mar", "2013", "16939"],
-	["Tea", "New York", "Apr", "2013", "9886"],
-	["Tea", "New York", "May", "2013", "15317"],
-	["Tea", "New York", "Jun", "2013", "12651"],
-	["Tea", "New York", "Jul", "2013", "16486"],
-	["Tea", "New York", "Aug", "2013", "12501"],
-	["Tea", "New York", "Sep", "2013", "12863"],
-	["Tea", "New York", "Oct", "2013", "16924"],
-	["Tea", "New York", "Nov", "2013", "14885"],
-	["Tea", "New York", "Dec", "2013", "9504"]
+    {
+        "Product": "Product",
+        "State": "State",
+        "Year": "Year",
+        "Sales": "Sales"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2013",
+        "Sales": "9550"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2013",
+        "Sales": "12446"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2013",
+        "Sales": "16939"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2014",
+        "Sales": "9886"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2014",
+        "Sales": "15317"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2014",
+        "Sales": "12651"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2015",
+        "Sales": "16486"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2015",
+        "Sales": "12501"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2015",
+        "Sales": "12863"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2016",
+        "Sales": "16924"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2016",
+        "Sales": "14885"
+    },
+    {
+        "Product": "Tea",
+        "State": "New York",
+        "Year": "2016",
+        "Sales": "9504"
+    }
 ];
 
 
 data2 = [
-	["Espresso", "New York", "Jan", "2013", "12451"],
-	["Espresso", "New York", "Feb", "2013", "14763"],
-	["Espresso", "New York", "Mar", "2013", "11748"],
-	["Espresso", "New York", "Apr", "2013", "13067"],
-	["Espresso", "New York", "May", "2013", "15864"],
-	["Espresso", "New York", "Jun", "2013", "12687"],
-	["Espresso", "New York", "Jul", "2013", "9911"],
-	["Espresso", "New York", "Aug", "2013", "12935"],
-	["Espresso", "New York", "Sep", "2013", "10759"],
-	["Espresso", "New York", "Oct", "2013", "9050"],
-	["Espresso", "New York", "Nov", "2013", "12860"],
-	"Espresso", "New York", "Dec", "2013", "13947"
+    {
+        "Product": "Product",
+        "State": "State",
+        "Year": "Year",
+        "Sales": "Sales"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2013",
+        "Sales": "12451"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2013",
+        "Sales": "14763"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2013",
+        "Sales": "11748"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2014",
+        "Sales": "13067"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2014",
+        "Sales": "15864"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2014",
+        "Sales": "12687"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2015",
+        "Sales": "9911"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2015",
+        "Sales": "12935"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2015",
+        "Sales": "10759"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2016",
+        "Sales": "9050"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2016",
+        "Sales": "12860"
+    },
+    {
+        "Product": "Espresso",
+        "State": "New York",
+        "Year": "2016",
+        "Sales": "13947"
+    }
 ]
+
+// arr = [];
+// for (i of data2) {
+// 	obj = {
+// 		Product: i[0],
+// 		State: i[1],
+// 		Year: i[2],
+// 		Sales: i[3]
+// 	}
+// 	arr.push(obj);
+// }
+// console.log(JSON.stringify(arr, null, 4))	
